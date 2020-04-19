94f2438e6d7fcaa68ca91d8153f4fc52c4901dc0
Paul Kang <paulkang@amazon.com>
2020-04-19 02:38:38 -0700

    Updates

---
+++ precache-manifest.1e9b72667b6ed329ace1ce782af86e91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3293c5005c3b4792c2a32ca241809416",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6258d8b16388cb935fe",
+    "url": "/static/css/main.3779c1c2.chunk.css"
+  },
+  {
+    "revision": "a5dc61fa57fff74f144e",
+    "url": "/static/js/2.fdabafab.chunk.js"
+  },
+  {
+    "revision": "61ba7d8397432dd9b09a150a080df22b",
+    "url": "/static/js/2.fdabafab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6258d8b16388cb935fe",
+    "url": "/static/js/main.2b923392.chunk.js"
+  },
+  {
+    "revision": "4885984d50a42209b6bb",
+    "url": "/static/js/runtime-main.f5e9834c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
